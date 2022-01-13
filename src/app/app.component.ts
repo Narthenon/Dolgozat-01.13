9e4551d1e213623d53374a033de8d7b3f58e4ca1
Narthenon <gbarabas20@gmail.com>
2022-01-13 10:23:20 +0100

    kész

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
+/*
+* File: ButtonPanel.java
+* Author: Barabás Gergő
+* Copyright: 2021, Barabás Gergő
+* Group: Szoft V
+* Date: 2021-01-13
+* Github: https://github.com/Narthenon/
+* Licenc: GNU GPL
+*/
+
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +18,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = '315';
+  teruletVisible=false;
+  trapezForm = new FormGroup( {
+    a: new FormControl(''),
+    b: new FormControl(''),
+    c: new FormControl(''),
+    d: new FormControl(''),
+    s: new FormControl(''),
+    terulet: new FormControl('')
+  });
+
+  onClickSzamit(){
+    let a = Number(this.trapezForm.value.a);
+    let b = Number(this.trapezForm.value.b);
+    let c = Number(this.trapezForm.value.c);
+    let d = Number(this.trapezForm.value.d);
+    let felker = (a+b+c+d)/2;
+    let terulet = ((felker-a)*(felker-b)*(felker-c)*(felker-d));
+    this.trapezForm.patchValue({terulet:terulet});
+    console.log (terulet)
+    this.teruletVisible = true;
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,15 @@
+/*
+* File: ButtonPanel.java
+* Author: Barabás Gergő
+* Copyright: 2021, Barabás Gergő
+* Group: Szoft V
+* Date: 2021-01-13
+* Github: https://github.com/Narthenon/
+* Licenc: GNU GPL
+*/
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
